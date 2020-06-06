cb3eea3c31f9525239413687087689aeed53b372
SeongJae Park <sj38.park@gmail.com>
2020-06-06 11:18:03 +0200

    Modularize version getting function
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- relstat.py
+++ relstat.py
@@ -3,6 +3,7 @@
 import argparse
 import os
 import subprocess
+import sys
 
 def cmd_str_output(cmd):
     output = subprocess.check_output(cmd)
@@ -14,6 +15,38 @@ def cmd_str_output(cmd):
 
 def gitcmd_str_output(cmd):
     return cmd_str_output(git_cmd + cmd)
+
+def get_versions():
+    versions_all = gitcmd_str_output(['tag']).split('\n')
+
+    versions = []
+    for version in versions_all:
+        # Only 'v[0-9]+.[0-9]+[-rc[0-9]+]' are valid by default
+        if not version.startswith('v'):
+            continue
+        version_numbers = version[1:].split('.')
+        if len(version_numbers) != 2:
+            continue
+        try:
+            major_version = int(version_numbers[0], 10)
+        except:
+            continue
+        minors = version_numbers[1].split('-rc')
+        if len(minors) > 2:
+            continue
+        try:
+            minor_version = int(minors[0], 10)
+            if len(minors) == 2:
+                rc = int(minors[1], 10)
+                if rc >= 999:
+                    print('rc >= 999 makes no sense')
+                    exit(1)
+            else:
+                rc = 999
+        except:
+            continue
+        versions.append([major_version, minor_version, rc])
+    return sorted(versions)
 
 def main():
     global git_cmd
@@ -30,31 +63,7 @@ def main():
     git_cmd = ['git', '--git-dir=%s' % args.gitdir]
 
     if not args.versions:
-        versions_all = gitcmd_str_output(['tag']).split('\n')
-
-        versions = []
-        for version in versions_all:
-            if not version.startswith('v'):
-                continue
-            version_numbers = version[1:].split('.')
-            if len(version_numbers) != 2:
-                continue
-            try:
-                major_version = int(version_numbers[0], 10)
-            except:
-                continue
-            minors = version_numbers[1].split('-rc')
-            if len(minors) > 2:
-                continue
-            try:
-                minor_version = int(minors[0], 10)
-                if len(minors) == 2:
-                    rc = int(minors[1], 10)
-                else:
-                    rc = None
-            except:
-                continue
-            versions.append([major_version, minor_version, rc])
+        versions = get_versions()
 
     for v in versions:
         print(v)
